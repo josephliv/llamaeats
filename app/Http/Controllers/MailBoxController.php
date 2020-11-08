c78fb40bb5e82100fcf75f4a205cc38d86bd73c0
gsantoswh <dyegofern@gmail.com>
2020-11-08 09:01:39 -0300

    Debbuging View

--- app/Http/Controllers/MailBoxController.php
+++ app/Http/Controllers/MailBoxController.php
@@ -230,14 +230,14 @@ class MailBoxController extends Controller
             $dateTo     = \Carbon\Carbon::parse($request->input('to-date'))->endOfDay();
         }
 
-        \DB::enableQueryLog();
+        //\DB::enableQueryLog();
         $leadMails = LeadMails::where('updated_at', '>=', $dateFrom)
                         ->where('updated_at', '<=', $dateTo)
                         ->orderBy('id', 'desc')->get();
 
-        $query = \DB::getQueryLog();
+        /*$query = \DB::getQueryLog();
         $query = end($query);
-        dd($query);
+        dd($query);*/
 
         return view('pages.emailsmanage', compact('leadMails', 'dateFrom', 'dateTo'));
 
