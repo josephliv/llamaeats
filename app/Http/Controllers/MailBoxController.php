13a90411587452f38b198dfb39e52114a4a4e57e
gsantoswh <dyegofern@gmail.com>
2020-11-08 08:56:32 -0300

    Debugging Query

--- app/Http/Controllers/MailBoxController.php
+++ app/Http/Controllers/MailBoxController.php
@@ -230,9 +230,15 @@ class MailBoxController extends Controller
             $dateTo     = \Carbon\Carbon::parse($request->input('to-date'))->endOfDay();
         }
 
+        DB::enableQueryLog();
         $leadMails = LeadMails::where('updated_at', '>=', $dateFrom)
                         ->where('updated_at', '<=', $dateTo)
                         ->orderBy('id', 'desc')->get();
+
+        $query = DB::getQueryLog();
+        $query = end($query);
+        dd($query);
+
         return view('pages.emailsmanage', compact('leadMails', 'dateFrom', 'dateTo'));
 
     }
